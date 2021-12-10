412d8bb82f5fae303039a981e761dcf90124c30b
Khouloud Jabloun <khouloudjabloun@MacBook-Air-de-Khouloud.local>
2021-12-11 00:42:57 +0100

    rename streamlit app

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from web3 import Web3
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 from etherscan import Etherscan
-import streamlit as st
-import numpy as np
-import igraph
-from igraph import Graph, EdgeSeq
-
-from src.scripts.detect_pkn_data import detect_params
+import app as st
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.style.use('ggplot')
